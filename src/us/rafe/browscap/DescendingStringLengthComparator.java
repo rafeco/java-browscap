9d23198a950b156eb31ec9a3a24675c037f1a5c9
Rafe Colburn <rcolburn@etsy.com>
2012-09-16 11:05:00 -0400

    comparator was sorting in the wrong order

--- src/us/rafe/browscap/DescendingStringLengthComparator.java
+++ src/us/rafe/browscap/DescendingStringLengthComparator.java
@@ -7,11 +7,11 @@ public class DescendingStringLengthComparator implements Comparator<String> {
     @Override
     public int compare(String arg0, String arg1) {
         if (arg0.length() < arg1.length()) {
-            return -1;
+            return 1;
         } else if (arg0.length() == arg1.length()) {
             return 0;
         } else {
-            return 1;
+            return -1;
         }
     }
 }
