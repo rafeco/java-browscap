5a9860ab95def0a46bb92778146f15363ec20dc9
Rafe Colburn <rcolburn@etsy.com>
2012-09-15 22:36:52 -0400

    comparator

--- test/us/rafe/browscap/TestBrowscap.java
+++ test/us/rafe/browscap/TestBrowscap.java
@@ -2,14 +2,12 @@ package us.rafe.browscap;
 
 import junit.framework.TestCase;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestBrowscap extends TestCase {
     static Logger logger = LoggerFactory.getLogger(TestBrowscap.class);
 
-    @Test
     public void testAndroid() {
         String ua = "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
 
@@ -25,7 +23,6 @@ public class TestBrowscap extends TestCase {
         assertEquals("Google", bc.getDeviceMaker());
     }
 
-    @Test
     public void testChrome16OSX() {
         String ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7";
 
@@ -38,7 +35,6 @@ public class TestBrowscap extends TestCase {
         assertEquals("MacOSX", bc.getPlatform());
     }
 
-    @Test
     public void testDefault() {
         String ua = "Not a real browser";
 
@@ -51,7 +47,6 @@ public class TestBrowscap extends TestCase {
         assertEquals("unknown", bc.getPlatform());
     }
 
-    @Test
     public void testIE9() {
         String ua = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)";
 
@@ -64,7 +59,6 @@ public class TestBrowscap extends TestCase {
         assertEquals("Win7", bc.getPlatform());
     }
 
-    @Test
     public void testIPad3() {
         String ua = "Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10";
 
@@ -80,7 +74,6 @@ public class TestBrowscap extends TestCase {
         assertEquals("Apple", bc.getDeviceMaker());
     }
 
-    @Test
     public void testIPhone() {
         String ua = "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3";
 
@@ -95,7 +88,6 @@ public class TestBrowscap extends TestCase {
         assertEquals("Apple", bc.getDeviceMaker());
     }
 
-    @Test
     public void testMaxthon3() {
         String ua = "Mozilla/5.0 (Windows; U; Windows NT 5.1; ) AppleWebKit/534.12 (KHTML, like Gecko) Maxthon/3.0 Safari/534.12";
 
@@ -108,7 +100,6 @@ public class TestBrowscap extends TestCase {
         assertEquals("WinXP", bc.getPlatform());
     }
 
-    @Test
     public void testSamsungS3() {
         String ua = "Mozilla/5.0 (Linux; U; Android 4.0.4; en-gb; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
 
